abc8a9803adba0dd14acb992a512dfef09e8f472
Song Byeong Geun <sbk8147@naver.com>
2020-09-24 14:37:54 +0900

    visible test

--- src/components/basicMenuBar/BasicMenuBar.tsx
+++ src/components/basicMenuBar/BasicMenuBar.tsx
@@ -5,7 +5,11 @@ import menuStyle from "./BasicMenuBar.module.scss";
 export default function BasicMenuBar(props: any) {
   return (
     <div className={menuStyle.container}>
-      <div className={menuStyle.top_container}></div>
+      <div className={menuStyle.top_container}>
+        <div className={menuStyle.menu_content}></div>
+        <div className={menuStyle.top_divider}></div>
+      </div>
+
       <div className={menuStyle.bottom_container}>
         <div className={menuStyle.side_container}>
           <div className={menuStyle.menu_container}>
