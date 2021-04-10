2be0ebbe3482862858883c69703d9b8a1e3fc575
Song Byeong Geun <sbk8147@naver.com>
2021-04-11 03:46:33 +0900

    first setting

--- src/page/signin/index.stories.tsx
+++ src/page/signin/index.stories.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import SignInPage from "./SignInPage";
+import UnauthRoute from "../../common/auth/UnauthRoute";
+import {
+  Switch,
+  Route,
+  Redirect,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
 
 export default { title: "SignIn" };
 
 export function basic() {
-  return <SignInPage />;
+  return <Router><Route><SignInPage /></Route></Router>;
 }
